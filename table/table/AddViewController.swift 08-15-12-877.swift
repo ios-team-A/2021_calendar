d4fc5f4c2702486a721530cf1586173f9590e9b6
yejin0928 <62542277+yejin0928@users.noreply.github.com>
2021-02-16 11:12:13 +0900

    Add files via upload

---
+++ table/table/AddViewController.swift 08-15-12-877.swift
@@ -0,0 +1,32 @@
+//
+//  AddViewController.swift
+//  table
+//
+//  Created by 권예진 on 2021/02/16.
+//
+
+import UIKit
+
+class AddViewController: UIViewController {
+
+    @IBOutlet var tfAddItem: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func btnAddItem(_ sender: UIButton) {
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
