c9b84b7ace2fd83fe69134d19a9f83ff0731a816
kms0524 <gcong2011@gmail.com>
2021-02-21 16:08:17 +0900

    0221

---
+++ 0218_calendar/TabBar_ios_team_a/CalendarDetailViewController.swift
@@ -0,0 +1,89 @@
+//
+//  CalendarDetailViewController.swift
+//  TabBar_ios_team_a
+//
+//  Created by 강민성 on 2021/02/18.
+//
+
+import UIKit
+
+class CalendarDetailViewController: UIViewController {
+    
+
+    @IBOutlet weak var workplaceImage: UIImageView!
+    @IBOutlet weak var houseImage: UIImageView!
+    @IBOutlet weak var calendarImage: UIImageView!
+    
+    @IBOutlet weak var workplaceLabel: UILabel!
+    @IBOutlet weak var houseLabel: UILabel!
+    @IBOutlet weak var calendarLabel: UILabel!
+    
+    @IBOutlet weak var cancelButton: UIButton!
+    @IBOutlet weak var saveButton: UIButton!
+    
+    @IBOutlet weak var workplaceButton: UIButton!
+    @IBOutlet weak var houseButton: UIButton!
+    @IBOutlet weak var calendarButton: UIButton!
+    
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var bodyTextField: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    @IBAction func save(_ sender: Any) {
+        if titleTextField.hasText == false || bodyTextField.hasText == false  {
+            let alert = UIAlertController(title: "저장 실패", message: "일정 제목과 내용을 입력해야 합니다.", preferredStyle: .alert)
+            let alertAction = UIAlertAction(title: "확인", style: .destructive, handler: nil)
+            alert.addAction(alertAction)
+            present(alert, animated: true, completion: nil)
+        } else if workplaceButton.isSelected == false &&
+            workplaceButton.isSelected == false &&
+            workplaceButton.isSelected == false {
+            let alert = UIAlertController(title: "저장 실패", message: "일정 종류를 선택해야 합니다.", preferredStyle: .alert)
+            let alertAction = UIAlertAction(title: "확인", style: .destructive, handler: nil)
+            alert.addAction(alertAction)
+            present(alert, animated: true, completion: nil)
+        } else {
+            //MARK: TODO : 일정 제목, 일정 내용, 일정 종류를 DB에 전송하기
+            dismiss(animated: true, completion: nil)
+            
+        }
+        
+    }
+    
+    @IBAction func cancel(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
+    @IBAction func didTapWorkplaceButton(_ sender: Any) {
+        workplaceButton.isSelected = !workplaceButton.isSelected
+        if houseButton.isSelected || calendarButton.isSelected {
+            houseButton.isSelected = false
+            calendarButton.isSelected = false
+        }
+    }
+    
+    @IBAction func didTapHouseButton(_ sender: Any) {
+        houseButton.isSelected = !houseButton.isSelected
+        if workplaceButton.isSelected || calendarButton.isSelected {
+            workplaceButton.isSelected = false
+            calendarButton.isSelected = false
+        }
+    }
+    
+    @IBAction func didTapCalendarButton(_ sender: Any) {
+        calendarButton.isSelected = !calendarButton.isSelected
+        if workplaceButton.isSelected || houseButton.isSelected {
+            workplaceButton.isSelected = false
+            houseButton.isSelected = false
+        }
+    }
+    
+    
+
+}
--- 0218_calendar/TabBar_ios_team_a/MainViewController.swift
+++ 0218_calendar/TabBar_ios_team_a/MainViewController.swift
@@ -8,74 +8,99 @@
 import UIKit
 
 class MainViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource ,UICollectionViewDelegateFlowLayout {
-    @IBOutlet weak var monthLabel: UILabel!
-    @IBOutlet weak var collectionView: UICollectionView!
+  
+  @IBOutlet weak var monthLabel: UILabel!
+  @IBOutlet weak var collectionView: UICollectionView!
+  
+  var selectedDate = Date()
+  var totalDates = [String]()
+  let calendarHelper = CalendarHelper()
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    setMonthView()
+  }
+  
+  func setMonthView() {
+    totalDates.removeAll()
+    let datesInMonth = calendarHelper.numOfDatesInMonth(date: selectedDate)
+    let firstDayOfMonth = calendarHelper.firstDayOfMonth(date: selectedDate)
+    let startingSpaces = calendarHelper.weekDay(date: firstDayOfMonth)
     
-    var selectedDate = Date()
-    var totalDates = [String]()
-    let calendarHelper = CalendarHelper()
+    var count: Int = 1
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        setMonthView()
+    while(count <= 42){
+      if count <= startingSpaces || count - startingSpaces > datesInMonth {
+        totalDates.append("")
+      }else{
+        totalDates.append("\(count-startingSpaces)")
+      }
+      count += 1
     }
     
-    func setMonthView() {
-        totalDates.removeAll()
-        let datesInMonth = calendarHelper.numOfDatesInMonth(date: selectedDate)
-        let firstDayOfMonth = calendarHelper.firstDayOfMonth(date: selectedDate)
-        let startingSpaces = calendarHelper.weekDay(date: firstDayOfMonth)
-        
-        var count: Int = 1
-        
-        while(count <= 42){
-            if count <= startingSpaces || count - startingSpaces > datesInMonth {
-                totalDates.append("")
-            }else{
-                totalDates.append("\(count-startingSpaces)")
-            }
-            count += 1
-        }
- 
-        monthLabel.text = calendarHelper.monthString(date: selectedDate) + "월 " + calendarHelper.yearString(date: selectedDate)
-        collectionView.reloadData()
+    monthLabel.text = calendarHelper.monthString(date: selectedDate) + "월 " + calendarHelper.yearString(date: selectedDate)
+    collectionView.reloadData()
+  }
+  
+  func collectionView(
+    _ collectionView: UICollectionView,
+    numberOfItemsInSection section: Int) -> Int {
+    return totalDates.count
+  }
+  
+  func collectionView(
+    _ collectionView: UICollectionView,
+    cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as? CalendarCell else {
+      return UICollectionViewCell()
     }
     
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return totalDates.count
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as? CalendarCell else {
-            return UICollectionViewCell()
-        }
-        cell.dateLabel.text = totalDates[indexPath.item]
-        return cell
-    }
-    
-    func collectionView(_ collectionView: UICollectionView,
-                          layout collectionViewLayout: UICollectionViewLayout,
-                          sizeForItemAt indexPath: IndexPath) -> CGSize {
-        let width = (collectionView.frame.size.width)/9
-        let height = width * 10/7
-        return CGSize(width: width, height: height)
-    }
+    cell.dateLabel.text = totalDates[indexPath.item]
 
-    func collectionView(_ collectionView: UICollectionView,
-                        didSelectItemAt indexPath: IndexPath) {
+    if cell.calendarDetailButton.isSelected {
+      selected()
     }
     
-    @IBAction func prevMonthBtn(_ sender: Any) {
-        selectedDate = calendarHelper.previousMonth(date: selectedDate)
-        setMonthView()
-    }
-    @IBAction func nextMonthBtn(_ sender: Any) {
-        selectedDate = calendarHelper.nextMonth(date: selectedDate)
-        setMonthView()
-    }
+    return cell
+  }
     
-
+  // MARK: cell을 선택했을 때 detailVC를 present하는 함수.
+  func selected() {
+    guard let detailVC = storyboard?.instantiateViewController(withIdentifier: "CalendarDetailViewController") as? CalendarDetailViewController else { return }
+    detailVC.modalPresentationStyle = .fullScreen
+    present(detailVC, animated: true)
+  }
+  
+  func collectionView(
+    _ collectionView: UICollectionView,
+    didDeselectItemAt indexPath: IndexPath) {
+  }
+  
+  func collectionView(
+    _ collectionView: UICollectionView,
+    layout collectionViewLayout: UICollectionViewLayout,
+    sizeForItemAt indexPath: IndexPath) -> CGSize {
+    let width = (collectionView.frame.size.width)/9
+    let height = width * 10/7
+    return CGSize(width: width, height: height)
+  }
+  
+  @IBAction func prevMonthBtn(_ sender: Any) {
+    selectedDate = calendarHelper.previousMonth(date: selectedDate)
+    setMonthView()
+  }
+  @IBAction func nextMonthBtn(_ sender: Any) {
+    selectedDate = calendarHelper.nextMonth(date: selectedDate)
+    setMonthView()
+  }
+  
+  // MARK: cell 안에 있는 버튼에 action 추가.
+  @IBAction func toDetialVC(_ sender: Any) {
+    selected()
+  }
 }
+
 class CalendarCell: UICollectionViewCell{
-    @IBOutlet weak var dateLabel: UILabel!
+  @IBOutlet weak var dateLabel: UILabel!
+  @IBOutlet weak var calendarDetailButton: UIButton!
 }
